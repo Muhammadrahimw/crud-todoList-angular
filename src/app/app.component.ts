9ad96de2be32ffc2db2d0a977d344b64121a9c12
Muhammadrahimw <muqalla21@gmail.com>
2025-08-26 18:56:29 +0500

    feat: update home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from "@angular/core";
+import {Router, RouterOutlet} from "@angular/router";
+import {MatTabsModule} from "@angular/material/tabs";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+	selector: "app-root",
+	imports: [RouterOutlet, MatTabsModule],
+	templateUrl: "./app.component.html",
+	styleUrl: "./app.component.css",
 })
 export class AppComponent {
-  title = 'second-app';
+	title = "second-app";
+	selectedIndex = 0;
+
+	constructor(private router: Router) {}
+
+	ngOnInit() {
+		if (this.router.url.includes("todo-list")) {
+			this.selectedIndex = 1;
+		} else {
+			this.selectedIndex = 0;
+		}
+	}
+
+	onTabChange(index: number) {
+		if (index === 0) {
+			this.router.navigate(["/food-list"]);
+		} else if (index === 1) {
+			this.router.navigate(["/todo-list"]);
+		}
+	}
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import {TodoListComponent} from "./todo-list/todo-list.component";
 import {FoodListPageComponent} from "./features/foods/pages/food-list-page/food-list-page.component";
 
 export const routes: Routes = [
+	{path: "", redirectTo: "/food-list", pathMatch: "full"},
 	{
 		path: `todo-list`,
 		component: TodoListComponent,
